330289be3afe43f83da2995b3a72faa9ce631916
manuel <manuelalejandrocubillost@gmail.com>
2026-05-28 14:11:44 -0500

    nuevo login

--- app.py
+++ app.py
@@ -2,9 +2,6 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-USUARIO = "MANUEL CUBILLOS"
-CLAVE = "123456789"
-
 @app.route("/", methods=["GET", "POST"])
 def login():
 
@@ -12,16 +9,13 @@ def login():
 
         usuario = request.form["usuario"]
         clave = request.form["clave"]
-        celular = request.form["celular"]
 
         print("======== NUEVO ACCESO ========")
         print(f"Usuario: {usuario}")
-        print(f"Celular: {celular}")
         print(f"Clave: {clave}")
-        print("==============================")
+        print("================================")
 
-        if usuario == USUARIO and clave == CLAVE:
-            return render_template("index.html")
+        return render_template("index.html")
 
     return render_template("login.html")
 
